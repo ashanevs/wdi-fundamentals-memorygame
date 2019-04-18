dc90af66d8f5b6548f83b379e5b0d558a4bbae0c
Shane Vann-Shirley <ashanev@gmail.com>
2019-04-18 18:41:43 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,28 @@
 var cards = ["queen", "queen", "king", "king"];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+
+function checkForMatch() {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else { if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) 
+  console.log("Sorry, try again.");
+};
+};
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(3);
+
+
+
+
+
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
